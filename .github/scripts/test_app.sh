985f433ee09befd9680dc2853e8d313351d5714a
marty <marty@martyulrich.com>
2022-02-12 16:30:54 -1000

    add verbose logging

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -5,4 +5,4 @@ set -eo pipefail
 xcodebuild -project iosApp/iosApp.xcodeproj \
             -scheme iosApp \
             -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
-            clean test | xcpretty
+            clean test -verbose | xcpretty
