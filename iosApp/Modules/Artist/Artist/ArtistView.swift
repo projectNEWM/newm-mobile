fcb7b20431afb7fdc98f941ecc342a8e359dc291
marty <marty@martyulrich.com>
2022-03-15 18:57:06 -0400

    modularization

---
+++ iosApp/Modules/Artist/Artist/ArtistView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ArtistView: DataView {
+	var id: String
+//	let viewModel: ArtistViewModel
+
+    var body: some View {
+		Text("Artist")
+    }
+}
+
+//struct ArtistView_Previews: PreviewProvider {
+//    static var previews: some View {
+//		ArtistView(artist: DummyData.makeArtist(name: "David Bowie"))
+//    }
+//}
--- iosApp/iosAppUITests/BottomNavigationUITests.swift
+++ iosApp/iosAppUITests/BottomNavigationUITests.swift
@@ -1,5 +1,4 @@
 import XCTest
-import iosApp
 
 class BottomNavigationUITests: XCTestCase {
     
