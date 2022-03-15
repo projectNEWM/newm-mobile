026d5156ba6ec115ed53cc4edc54ae9963841419
marty <marty@martyulrich.com>
2022-03-15 18:57:06 -0400

    modularization

--- iosApp/iosApp/App/iOSApp.swift
+++ iosApp/iosApp/App/iOSApp.swift
@@ -3,17 +3,14 @@ import NEWMApp
 
 @main
 struct iOSApp: App {
-//	@StateObject var viewModel = iOSAppViewModel()
-//
-//	init() {
-//		UINavigationBar.appearance().tintColor = .white
-//#if DEBUG
-//		UserDefaults.standard.set(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
-//#endif
-//	}
-//
+	init() {
+		UINavigationBar.appearance().tintColor = .white
+#if DEBUG
+		UserDefaults.standard.set(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
+#endif
+	}
+	
 	var body: some Scene {
-//		NEWMApp.iOSApp()
 		WindowGroup {
 			NEWMApp()
 		}
