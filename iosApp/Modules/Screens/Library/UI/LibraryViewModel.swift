4ee45b55dd0ec9da26c782e18138a8c5834ac6a9
Dalton Ulrich <daltonulrich2002@gmail.com>
2022-08-31 10:19:02 -0700

    tab indentation fixed

--- iosApp/Modules/Screens/Library/UI/LibraryViewModel.swift
+++ iosApp/Modules/Screens/Library/UI/LibraryViewModel.swift
@@ -7,40 +7,40 @@ import Utilities
 import SharedUI
 
 class LibraryViewModel: ObservableObject {
-    @MainActor @Published var state: ViewState<(LibraryViewUIModel, LibraryViewActionHandling)> = .loading
-    @Published var route: LibraryRoute?
-    
-    @Injected private var uiModelProvider: LibraryViewUIModelProviding
-    
-    init() {
-        Task {
-            await refresh()
-        }
-    }
-    
-    @MainActor
-    func refresh() async {
-        do {
-            state = .loading
-            let uiModel = try await uiModelProvider.getModel()
-            state = .loaded((uiModel, self))
-        } catch {
-            state = .error(error)
-        }
-    }
-    
+	@MainActor @Published var state: ViewState<(LibraryViewUIModel, LibraryViewActionHandling)> = .loading
+	@Published var route: LibraryRoute?
+	
+	@Injected private var uiModelProvider: LibraryViewUIModelProviding
+	
+	init() {
+		Task {
+			await refresh()
+		}
+	}
+	
+	@MainActor
+	func refresh() async {
+		do {
+			state = .loading
+			let uiModel = try await uiModelProvider.getModel()
+			state = .loaded((uiModel, self))
+		} catch {
+			state = .error(error)
+		}
+	}
+	
 }
 
 extension LibraryViewModel: LibraryViewActionHandling {
-    func artistTapped(id: String) {
-        print(#function + " " + id)
-        route = .artist(id: id)
-    }
-    
-    func songTapped(id: String) {
-        print(#function + " " + id)
-        route = .songPlaying(id: id)
-    }
+	func artistTapped(id: String) {
+		print(#function + " " + id)
+		route = .artist(id: id)
+	}
+	
+	func songTapped(id: String) {
+		print(#function + " " + id)
+		route = .songPlaying(id: id)
+	}
 	
 	func playlistTapped(id: String) {
 		print(#function + " " + id)
