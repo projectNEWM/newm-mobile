9a5117747eb688057933cdd3dadb7083c4a27108
marty <marty@martyulrich.com>
2022-08-31 19:16:54 -0700

    Ktor needs to be public I guess for Android

--- buildSrc/src/main/java/Ktor.kt
+++ buildSrc/src/main/java/Ktor.kt
@@ -1,4 +1,4 @@
-internal object Ktor {
+object Ktor {
     const val android = "io.ktor:ktor-client-android:${Versions.ktor}"
     const val clientSerialization = "io.ktor:ktor-client-serialization:${Versions.ktor}"
     const val core = "io.ktor:ktor-client-core:${Versions.ktor}"
