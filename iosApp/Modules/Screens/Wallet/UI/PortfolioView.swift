e360c8a64df944b01087cc8b7a82d13ebcbd85ce
marty <marty@martyulrich.com>
2022-12-20 13:52:23 -0800

    Tweak Portfolio section

--- iosApp/Modules/Screens/Wallet/UI/PortfolioView.swift
+++ iosApp/Modules/Screens/Wallet/UI/PortfolioView.swift
@@ -21,17 +21,18 @@ extension PortfolioView {
 	@ViewBuilder
 	private var pickerButton: some View {
 		PickerButton(label: model.pickerLabel)
-			.padding(.bottom)
+			.padding([.bottom, .trailing], sidePadding/2)
 			.foregroundColor(NEWMColor.grey100.swiftUIColor)
 	}
 	
 	@ViewBuilder
 	private var header: some View {
 		HStack {
-			Text(model.songHeaderTitle).font(.inter(ofSize: 14).bold())
+			Text(model.songHeaderTitle).font(.inter(ofSize: 14).weight(.semibold))
 			Spacer()
 			Text(model.royaltyTitle).font(.inter(ofSize: 14).bold())
 		}
+		.padding([.leading, .trailing], sidePadding/2)
 	}
 	
 	@ViewBuilder
@@ -51,13 +52,14 @@ extension PortfolioView {
 				image.circle(size: size)
 			} placeholder: {
 				Image.placeholder.circle(size: size)
-			}
+			}.padding(.leading, sidePadding/2)
 			Text(model.title)
 			Spacer()
 			Text(model.price)
+				.padding(.trailing, sidePadding/2)
 		}
 		.font(.inter(ofSize: 12).weight(.medium))
-		.padding(8)
+		.padding([.top, .bottom], 8)
 		.background(index % 2 == 0 ? NEWMColor.grey600.swiftUIColor : .black)
 	}
 }
@@ -66,5 +68,6 @@ struct PortfolioSection_Previews: PreviewProvider {
 	static var previews: some View {
 		PortfolioView(model: Resolver.resolve(WalletViewModel.self).portfolioSection)
 			.preferredColorScheme(.dark)
+			.previewLayout(.fixed(width: 390, height: 844))
 	}
 }
