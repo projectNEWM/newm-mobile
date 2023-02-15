321d3402af0740085f4c544d76ec69e749b032a8
Jose Mateo <jmateo6@gmail.com>
2023-02-15 10:12:08 -0500

    Initial setup for paparazzi

---
+++ android/feature-login/src/test/kotlin/io/newm/feature/login/screen/CreateAccountScreenTest.kt
@@ -0,0 +1,64 @@
+package io.newm.feature.login.screen
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.DeviceConfig.Companion.PIXEL_2
+import app.cash.paparazzi.DeviceConfig.Companion.PIXEL_6
+import app.cash.paparazzi.Paparazzi
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import io.newm.core.theme.NewmTheme
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class CreateAccountScreenTest(
+    @TestParameter private val theme: TestTheme,
+    @TestParameter testDevice: TestDevice,
+    @TestParameter fontScale: FontScale,
+) {
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = testDevice.deviceConfig.copy(fontScale = fontScale.scale),
+        theme = "android:Theme.Material.Light.NoActionBar"
+    )
+
+    @Test
+    fun default() {
+        paparazzi.snapshot {
+            NewmTheme(
+                darkTheme = theme == TestTheme.Dark
+            ) {
+                CreateAccountScreen(
+                    userState = CreateAccountViewModel.SignupUserState(),
+                    onNext = {},
+                    onUserLoggedIn = {},
+                    setUserPasswordConfirmation = {},
+                    setUserPassword = {},
+                    requestCode = {},
+                    setUserEmail = {}
+                )
+            }
+        }
+    }
+}
+
+// TODO move to a different file to use in other test
+enum class TestTheme {
+    Dark,
+    Light,
+}
+
+enum class TestDevice(
+    val deviceConfig: DeviceConfig,
+) {
+    Pixel2(PIXEL_2),
+    Pixel6(PIXEL_6)
+}
+
+enum class FontScale(
+    val scale: Float
+) {
+    NORMAL(scale = 1f),
+    LARGE(scale = 2f)
+}
