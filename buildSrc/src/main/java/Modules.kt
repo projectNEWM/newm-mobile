7cbeca5c6a1da0ce931a75e86b5d44d1660e5f09
Cristhian Escobar <escobarcristhian@utexas.edu>
2023-09-13 00:14:10 -0500

    Restructure modules to separate features from core libraries

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -1,10 +1,10 @@
 object Modules {
-    const val coreTheme = ":android:core-theme"
-    const val coreUiUtils = ":android:core-ui-utils"
-    const val coreResources = ":android:core-resources"
+    const val coreTheme = ":android:core:theme"
+    const val coreUiUtils = ":android:core:ui-utils"
+    const val coreResources = ":android:core:resources"
 
-    const val nowPlaying = ":android:feature-now-playing"
-    const val login = ":android:feature-login"
+    const val nowPlaying = ":android:features:now-playing"
+    const val login = ":android:features:login"
     const val shared = ":shared"
-    const val testUtils = ":android:core-test-utils"
+    const val testUtils = ":android:core:test-utils"
 }
