9a8b60016025721a7a26111bc9b514eac615653c
CJ <cristhianescobar@users.noreply.github.com>
2023-10-26 17:14:47 -0500

    Parse Music Metadata V2 (#148)

--- shared/src/commonMain/kotlin/io.newm.shared/usecases/WalletNFTSongsUseCase.kt
+++ shared/src/commonMain/kotlin/io.newm.shared/usecases/WalletNFTSongsUseCase.kt
@@ -4,6 +4,7 @@ import co.touchlab.kermit.Logger
 import io.newm.shared.login.repository.LogInRepository
 import io.newm.shared.models.Song
 import io.newm.shared.repositories.CardanoWalletRepository
+import io.newm.shared.repositories.NFTTrack
 import io.newm.shared.repositories.testdata.MockSongs
 import io.newm.shared.services.LedgerAssetMetadata
 import io.newm.shared.services.UserAPI
@@ -13,13 +14,13 @@ import org.koin.core.component.inject
 interface WalletNFTSongsUseCase {
     fun getAllWalletNFTSongs(xPub: String): Flow<List<Song>>
 
-    suspend fun getWalletNFTs(xpub: String): List<LedgerAssetMetadata>
+    suspend fun getWalletNFTs(xpub: String): List<NFTTrack>
 }
 
 internal class WalletNFTSongsUseCaseImpl(private val repository: CardanoWalletRepository) : WalletNFTSongsUseCase {
 
     override fun getAllWalletNFTSongs(xPub: String): Flow<List<Song>> = MockSongs.getSongsFlow()
-    override suspend fun getWalletNFTs(xpub: String): List<LedgerAssetMetadata> {
+    override suspend fun getWalletNFTs(xpub: String): List<NFTTrack> {
         return repository.getWalletNFTs(xpub)
     }
 }
