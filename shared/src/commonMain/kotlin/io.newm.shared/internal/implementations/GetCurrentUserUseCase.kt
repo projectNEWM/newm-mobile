ed6bd5b7b980c7867ad33e38e16742a2a908e8db
marty <marty@martyulrich.com>
2023-11-09 14:56:16 +0900

    fix bad merge

--- shared/src/commonMain/kotlin/io.newm.shared/internal/implementations/GetCurrentUserUseCase.kt
+++ shared/src/commonMain/kotlin/io.newm.shared/internal/implementations/GetCurrentUserUseCase.kt
@@ -1,14 +1,14 @@
-package io.newm.shared.usecases
-
-import io.newm.shared.models.User
-import io.newm.shared.repositories.UserRepository
-
-interface GetCurrentUserUseCase {
-    suspend fun execute(): User
-}
-
-internal class GetCurrentUserUseCaseImpl(private val repository: UserRepository): GetCurrentUserUseCase {
-    override suspend fun execute(): User {
-        return repository.getCurrentUser()
-    }
-}
+//package io.newm.shared.usecases
+//
+//import io.newm.shared.models.User
+//import io.newm.shared.repositories.UserRepository
+//
+//interface GetCurrentUserUseCase {
+//    suspend fun execute(): User
+//}
+//
+//internal class GetCurrentUserUseCaseImpl(private val repository: UserRepository): GetCurrentUserUseCase {
+//    override suspend fun execute(): User {
+//        return repository.getCurrentUser()
+//    }
+//}
