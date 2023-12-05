3d7d5a326f542949b7294db086e203730ca97dc3
Marty Ulrich <marty@martyulrich.com>
2023-12-06 01:52:51 +0900

    build updates

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -34,7 +34,7 @@ kotlin {
     val xcf = XCFramework()
     listOf(
 //        iosX64(),
-        iosArm64(),
+//        iosArm64(),
         iosSimulatorArm64()
     ).forEach {
         it.binaries.framework {
@@ -84,12 +84,12 @@ kotlin {
         }
 
 //        val iosX64Main by getting
-        val iosArm64Main by getting
+//        val iosArm64Main by getting
         val iosSimulatorArm64Main by getting
         val iosMain by creating {
             dependsOn(commonMain)
 //            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
+//            iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
             dependencies {
                 implementation(Ktor.iosDarwin)
@@ -97,12 +97,12 @@ kotlin {
             }
         }
 //        val iosX64Test by getting
-        val iosArm64Test by getting
+//        val iosArm64Test by getting
         val iosSimulatorArm64Test by getting
         val iosTest by creating {
             dependsOn(commonTest)
 //            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
+//            iosArm64Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
         }
     }
