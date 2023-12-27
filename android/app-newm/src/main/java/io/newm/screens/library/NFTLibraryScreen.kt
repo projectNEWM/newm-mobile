3fda5695bf8956138530bbfe55980df74adb2a22
CJ <cristhianescobar@users.noreply.github.com>
2023-12-27 14:52:52 -0600

    Update NFT Lib screen (#172)

--- android/app-newm/src/main/java/io/newm/screens/library/NFTLibraryScreen.kt
+++ android/app-newm/src/main/java/io/newm/screens/library/NFTLibraryScreen.kt
@@ -2,6 +2,7 @@ package io.newm.screens.library
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -13,17 +14,20 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -31,7 +35,8 @@ import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import io.newm.core.resources.R
 import io.newm.core.theme.CerisePink
-import io.newm.core.theme.Gray100
+import io.newm.core.theme.Gray16
+import io.newm.core.theme.GraySuit
 import io.newm.core.theme.SteelPink
 import io.newm.core.theme.White
 import io.newm.core.theme.inter
@@ -39,6 +44,8 @@ import io.newm.core.theme.raleway
 import io.newm.core.ui.LoadingScreen
 import io.newm.core.ui.text.SearchBar
 import io.newm.core.ui.utils.ErrorScreen
+import io.newm.core.ui.utils.drawWithBrush
+import io.newm.core.ui.utils.secondsToMinutesSecondsString
 import io.newm.core.ui.utils.textGradient
 import io.newm.feature.musicplayer.MiniPlayer
 import io.newm.screens.library.screens.EmptyWalletScreen
@@ -49,7 +56,6 @@ import org.koin.compose.koinInject
 
 internal const val TAG_NFT_LIBRARY_SCREEN = "TAG_NFT_LIBRARY_SCREEN"
 
-@OptIn(ExperimentalTextApi::class)
 @Composable
 fun NFTLibraryScreen(
     onPlaySong: (NFTTrack) -> Unit,
@@ -109,18 +115,48 @@ fun NFTTracks(
                 .verticalScroll(state = rememberScrollState())
                 .testTag(TAG_NFT_LIBRARY_SCREEN)
         ) {
-            SearchBar(
-                placeholderResId = R.string.library_search,
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(vertical = 16.dp),
-                onQueryChange = onQueryChange
-            )
+                    .padding(bottom = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                SearchBar(
+                    placeholderResId = R.string.library_search,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    onQueryChange = onQueryChange
+                )
+                IconButton(
+                    modifier = Modifier
+                        .padding(top = 10.dp, bottom = 10.dp, start = 16.dp),
+                    onClick = {/*TODO*/ }) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_library_filter),
+                        contentDescription = "Filter",
+                        modifier = Modifier
+                            .drawWithBrush(LibraryBrush)
+
+                    )
+                }
+            }
             when {
                 showZeroResultsFound -> ZeroSearchResults()
                 tracks.isNotEmpty() -> {
-                    tracks.forEach { song ->
-                        RowSongItem(song = song, onClick = onPlaySong)
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(Gray16)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .padding(horizontal = 16.dp, vertical = 8.dp)
+                        ) {
+                            tracks.forEach { song ->
+                                RowSongItem(song = song, onClick = onPlaySong)
+                            }
+                        }
                     }
                 }
             }
@@ -142,14 +178,14 @@ private fun RowSongItem(
 ) {
     Row(
         modifier = Modifier
-            .padding(vertical = 6.dp)
+            .padding(vertical = 8.dp)
             .fillMaxWidth()
             .clickable(onClick = { onClick(song) })
     ) {
         AsyncImage(
             model = song.imageUrl,
             modifier = Modifier
-                .size(68.dp)
+                .size(40.dp)
                 .clip(RoundedCornerShape(4.dp)),
             contentScale = ContentScale.Crop,
             contentDescription = null,
@@ -159,21 +195,28 @@ private fun RowSongItem(
                 .padding(start = 12.dp)
         ) {
             Text(
-                modifier = Modifier.padding(top = 8.dp),
                 text = song.title,
                 fontFamily = inter,
-                fontWeight = FontWeight.Bold,
-                fontSize = 12.sp,
+                fontWeight = FontWeight.Medium,
+                fontSize = 14.sp,
                 color = White
             )
             Text(
-                text = song.artists.joinToString(separator = ", "),
+                text = song.artists.joinToString(","),
                 fontFamily = inter,
                 fontWeight = FontWeight.Normal,
                 fontSize = 12.sp,
-                color = Gray100
+                color = GraySuit
             )
         }
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = song.duration.secondsToMinutesSecondsString(),
+            fontFamily = inter,
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp,
+            color = GraySuit
+        )
     }
 }
 
