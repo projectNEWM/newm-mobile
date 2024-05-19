7d238e0669969642af8ebd9b78369721712b6c11
Cristhian Escobar <escobarcristhian@utexas.edu>
2024-05-19 17:40:32 -0500

    PR Feedback

--- shared/src/commonMain/kotlin/io.newm.shared/internal/services/RemoteConfigAPI.kt
+++ shared/src/commonMain/kotlin/io.newm.shared/internal/services/RemoteConfigAPI.kt
@@ -2,9 +2,9 @@ package io.newm.shared.internal.services
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.accept
 import io.ktor.client.request.get
 import io.ktor.http.ContentType
-import io.ktor.http.contentType
 import io.newm.shared.di.NetworkClientFactory
 import io.newm.shared.internal.services.models.MobileConfig
 import org.koin.core.component.KoinComponent
@@ -15,6 +15,6 @@ internal class RemoteConfigAPI(networkClient: NetworkClientFactory) : KoinCompon
 
     suspend fun getMobileConfig(): MobileConfig =
         httpClient.get("v1/mobile-config") {
-            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
         }.body()
 }
