4e4bbf234a4d9ae7f96d7b55eabfd96b441cdf03
newmskywalker <3849278+newmskywalker@users.noreply.github.com>
2024-07-20 21:30:34 -0700

    Revert circuit upgrade (#292)

--- android/app-newm/src/main/java/io/newm/NewmNavigator.kt
+++ android/app-newm/src/main/java/io/newm/NewmNavigator.kt
@@ -3,12 +3,10 @@ package io.newm
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import com.slack.circuit.runtime.Navigator
-import com.slack.circuit.runtime.screen.PopResult
 import com.slack.circuit.runtime.screen.Screen
 import io.newm.feature.login.screen.HomeScreen
 import io.newm.screens.WebBrowserScreen
 import io.newm.shared.NewmAppLogger
-import kotlinx.collections.immutable.ImmutableList
 
 
 @Composable
@@ -26,34 +24,24 @@ private class NewmNavigator(
     private val logger: NewmAppLogger,
     private val startHomeActivity: () -> Unit,
     private val launchBrowser: (String) -> Unit,
-) : Navigator by circuitNavigator {
-    override fun goTo(screen: Screen) : Boolean {
-        logger.debug(tag = "NewmNavigator", message = "Navigating to $screen")
-        return when (screen) {
-            is HomeScreen -> {
-                startHomeActivity()
-                true
-            }
-            is WebBrowserScreen -> {
-                launchBrowser(screen.url)
-                true
-            }
+) : Navigator {
+    override fun goTo(screen: Screen) {
+        logger.debug(tag = "NewmNavigator", message = "Navigating to $screen with $circuitNavigator")
+        when (screen) {
+            is HomeScreen -> startHomeActivity()
+            is WebBrowserScreen -> launchBrowser(screen.url)
             else -> circuitNavigator.goTo(screen)
         }
     }
 
-    override fun resetRoot(
-        newRoot: Screen,
-        saveState: Boolean,
-        restoreState: Boolean
-    ): ImmutableList<Screen> {
-        logger.debug(tag = "NewmNavigator", message = "Resetting root to $newRoot")
-        return circuitNavigator.resetRoot(newRoot, saveState, restoreState)
-    }
-
-    override fun pop(result: PopResult?): Screen? {
+    override fun pop(): Screen? {
         val screen = circuitNavigator.pop()
         logger.debug(tag = "NewmNavigator", message = "Popping screen: $screen")
         return screen
+    }
+
+    override fun resetRoot(newRoot: Screen): List<Screen> {
+        logger.debug(tag = "NewmNavigator", message = "Resetting root to $newRoot")
+        return circuitNavigator.resetRoot(newRoot)
     }
 }
