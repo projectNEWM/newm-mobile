19a696e671b2960e78adbdef43ff75ae188945fd
newmskywalker <3849278+newmskywalker@users.noreply.github.com>
2024-08-04 20:35:30 -0700

    Add Store5 and create a store for nft tracks (#302)
    * Add Store5 and create a store for nft tracks
    
    * Add pull to refresh (#303)

--- android/app-newm/src/main/java/io/newm/screens/library/NFTLibraryEvent.kt
+++ android/app-newm/src/main/java/io/newm/screens/library/NFTLibraryEvent.kt
@@ -4,6 +4,8 @@ import com.slack.circuit.runtime.CircuitUiEvent
 import io.newm.shared.public.models.NFTTrack
 
 sealed interface NFTLibraryEvent : CircuitUiEvent {
+    data object OnRefresh : NFTLibraryEvent
+
     data class PlaySong(val track: NFTTrack) : NFTLibraryEvent
 
     data class OnQueryChange(val newQuery: String) : NFTLibraryEvent
