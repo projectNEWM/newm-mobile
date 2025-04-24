cdb6bbbc7c37bd1ad559139b300320f47c7426a6
newmskywalker <3849278+newmskywalker@users.noreply.github.com>
2025-04-23 20:13:46 -0400

    Upgrade to ktor3 to get web app building

--- composeapp/build.gradle.kts
+++ composeapp/build.gradle.kts
@@ -15,7 +15,6 @@ plugins {
 
 kotlin {
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
