8472576e0ce5d538de4e6bf2c7ae1685e8f2e173
newmskywalker <3849278+newmskywalker@users.noreply.github.com>
2025-04-23 20:35:23 -0400

    Add dynamic status bar back to android

---
+++ android/app-newm/src/main/java/io/newm/utils/DynamicStatusBarSideEffect.kt
@@ -0,0 +1,25 @@
+package io.newm.utils
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+@Composable
+fun DynamicStatusBarSideEffect(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+) {
+    val view = LocalView.current
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).apply {
+                isAppearanceLightStatusBars = !darkTheme
+                isAppearanceLightNavigationBars = !darkTheme
+            }
+        }
+    }
+}
