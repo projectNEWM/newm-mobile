801040e154e59836da64e16b05fbd232f456beea
Cristhian Escobar <escobarcristhian@utexas.edu>
2025-05-28 17:37:29 -0500

    Library version updates

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,142 +1,173 @@
-
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
 import org.jetbrains.kotlin.konan.properties.Properties
 import java.io.FileInputStream
 
 plugins {
-	kotlin("multiplatform")
-	kotlin("plugin.serialization")
-	id("kotlinx-serialization")
-	id("com.android.library")
-	id("com.squareup.sqldelight")
-	alias(libs.plugins.ksp)
-	id("com.github.gmazzo.buildconfig") version "5.3.5"
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    id("kotlinx-serialization")
+    id("com.android.library")
+    id("com.squareup.sqldelight")
+    alias(libs.plugins.ksp)
+    id("com.github.gmazzo.buildconfig") version "5.6.5"
 }
 
 android {
-	compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
 
-	sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-	namespace = "io.newm.shared"
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    namespace = "io.newm.shared"
 
-	defaultConfig {
-		minSdk = libs.versions.android.minSdk.get().toInt()
-		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-	}
-	compileOptions {
-		sourceCompatibility = JavaVersion.VERSION_11
-		targetCompatibility = JavaVersion.VERSION_11
-	}
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
 }
 
 kotlin {
-	androidTarget()
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
+    }
+    jvm {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
+    }
 
-	val xcf = XCFramework()
-	listOf(
-		iosArm64(),
-		iosSimulatorArm64()
-	).forEach {
-		it.binaries.framework {
-			baseName = "shared"
-			xcf.add(this)
-		}
-	}
+    val xcf = XCFramework()
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "shared"
+            xcf.add(this)
+        }
+    }
 
-	sourceSets {
-		val commonMain by getting {
-			dependencies {
-				implementation(libs.kotlinx.coroutines.core)
-				implementation(libs.kotlin.stdlib)
-				implementation(libs.runtime)
-				implementation(libs.coroutines.extensions)
-				api(libs.koin.core)
-				implementation(libs.ktor.client.logging)
-				implementation(libs.ktor.client.core)
-				implementation(libs.ktor.client.cio)
-				implementation(libs.ktor.client.content.negotiation)
-				implementation(libs.ktor.serialization.kotlinx.json)
-				implementation(libs.ktor.client.auth)
-				implementation(libs.androidx.datastore.preferences)
-				implementation(libs.store5)
-				implementation(libs.kvault)
-			}
-		}
-		val commonTest by getting {
-			dependencies {
-				implementation(libs.koin.test)
-				implementation(libs.kotlinx.coroutines.test)
-				implementation(kotlin("test-common"))
-				implementation(kotlin("test-annotations-common"))
-			}
-		}
-		val androidMain by getting {
-			dependencies {
-				implementation(libs.android.driver)
-				implementation(libs.ktor.client.android)
-				implementation(libs.cloudinary.android)
-			}
-		}
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.kotlin.stdlib)
+                implementation(libs.runtime)
+                implementation(libs.coroutines.extensions)
+                api(libs.koin.core)
+                implementation(libs.ktor.client.logging)
+                implementation(libs.ktor.client.core)
+                implementation(libs.ktor.client.cio)
+                implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.serialization.kotlinx.json)
+                implementation(libs.ktor.client.auth)
+                implementation(libs.androidx.datastore.preferences)
+                implementation(libs.store5)
+                implementation(libs.kvault)
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.koin.test)
+                implementation(libs.kotlinx.coroutines.test)
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation(libs.android.driver)
+                implementation(libs.ktor.client.android)
+                implementation(libs.cloudinary.android)
+            }
+        }
 
-		named("androidUnitTest") {
-			dependencies {
-				implementation(kotlin("test-junit"))
-				implementation(libs.junit)
-			}
-		}
+        named("androidUnitTest") {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation(libs.junit)
+            }
+        }
 
-		val iosArm64Main by getting
-		val iosSimulatorArm64Main by getting
-		val iosMain by creating {
-			dependsOn(commonMain)
-			iosArm64Main.dependsOn(this)
-			iosSimulatorArm64Main.dependsOn(this)
-			dependencies {
-				implementation(libs.ktor.client.darwin)
-				implementation(libs.native.driver)
-			}
-		}
-		val iosSimulatorArm64Test by getting
-		val iosTest by creating {
-			dependsOn(commonTest)
-			iosSimulatorArm64Test.dependsOn(this)
-		}
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation(libs.ktor.client.darwin)
+                implementation(libs.native.driver)
+            }
+        }
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
 
-		all {
-			languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
-		}
-	}
+        all {
+            languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
+        }
+    }
 }
 
 buildConfig {
-	packageName("io.newm.shared.generated")
+    packageName("io.newm.shared.generated")
 
-	val properties = Properties().apply { load(FileInputStream(File(rootProject.rootDir, "local.properties"))) }
+    val properties = Properties().apply {
+        load(FileInputStream(File(rootProject.rootDir, "local.properties")))
+    }
 
-	buildConfigField<String>( "STAGING_URL", properties.getProperty("STAGING_URL").replace("\"", ""))
-	buildConfigField<String>("PRODUCTION_URL", properties.getProperty("PRODUCTION_URL").replace("\"", ""))
-	buildConfigField<String>("GOOGLE_AUTH_CLIENT_ID", properties.getProperty("GOOGLE_AUTH_CLIENT_ID").replace("\"", ""))
-	buildConfigField<String>("RECAPTCHA_SITE_KEY", properties.getProperty("RECAPTCHA_SITE_KEY").replace("\"", ""))
-	buildConfigField<String>("SENTRY_AUTH_TOKEN", properties.getProperty("SENTRY_AUTH_TOKEN").replace("\"", ""))
-	buildConfigField<String>("ANDROID_SENTRY_DSN", properties.getProperty("ANDROID_SENTRY_DSN").replace("\"", ""))
-	buildConfigField<String>("LAUNCHDARKLY_MOBILE_KEY", properties.getProperty("LAUNCHDARKLY_MOBILE_KEY").replace("\"", ""))
-}
-
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-	kotlinOptions {
-		jvmTarget = "11"
-	}
+    buildConfigField(
+        type = "String",
+        name = "STAGING_URL",
+        expression = properties.getProperty("STAGING_URL")
+    )
+    buildConfigField(
+        type = "String",
+        name = "PRODUCTION_URL",
+        properties.getProperty("PRODUCTION_URL")
+    )
+    buildConfigField(
+        type = "String",
+        name = "GOOGLE_AUTH_CLIENT_ID",
+        properties.getProperty("GOOGLE_AUTH_CLIENT_ID")
+    )
+    buildConfigField(
+        type = "String",
+        name = "RECAPTCHA_SITE_KEY",
+        properties.getProperty("RECAPTCHA_SITE_KEY")
+    )
+    buildConfigField(
+        type = "String",
+        name = "SENTRY_AUTH_TOKEN",
+        properties.getProperty("SENTRY_AUTH_TOKEN")
+    )
+    buildConfigField(
+        type = "String",
+        name = "ANDROID_SENTRY_DSN",
+        properties.getProperty("ANDROID_SENTRY_DSN")
+    )
+    buildConfigField(
+        type = "String",
+        name = "LAUNCHDARKLY_MOBILE_KEY",
+        properties.getProperty("LAUNCHDARKLY_MOBILE_KEY")
+    )
 }
 
 sqldelight {
-	database("NewmDatabase") {
-		packageName = "io.newm.shared.db.cache"
-		sourceFolders = listOf("sqldelight")
-		version = 4
-	}
+    database("NewmDatabase") {
+        packageName = "io.newm.shared.db.cache"
+        sourceFolders = listOf("sqldelight")
+        version = 4
+    }
 }
 
 kotlin.sourceSets.all {
-	languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
+    languageSettings.optIn("kotlin.experimental.ExperimentalObjCName")
 }
