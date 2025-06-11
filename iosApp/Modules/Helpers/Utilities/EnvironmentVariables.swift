2807e17d354e19f62934eaea91339b142df37c54
marty <marty@martyulrich.com>
2025-06-11 17:21:20 +0900

    make env var readable

--- iosApp/Modules/Helpers/Utilities/EnvironmentVariables.swift
+++ iosApp/Modules/Helpers/Utilities/EnvironmentVariables.swift
@@ -6,10 +6,10 @@ public enum EnvironmentVariable: String {
 	case recaptchaKey = "RECAPTCHA_KEY_ID"
 
 	public var value: String {
-		if let value = ProcessInfo.processInfo.environment[rawValue] {
+		if let value = Bundle.main.object(forInfoDictionaryKey: rawValue) as? String {
 			return value
 		} else {
-			Resolver.resolve(ErrorReporting.self).logError("Config var not found: \(rawValue).\nEnvironment: \(ProcessInfo.processInfo.environment)")
+			Resolver.resolve(ErrorReporting.self).logError("Config var not found: \(rawValue)")
 			return ""
 		}
 	}
